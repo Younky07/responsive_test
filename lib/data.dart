fc211a0a14595e98091564bf0387103ba160899d
Black07Jack <Black07Jack@users.noreply.github.com>
2022-10-17 22:52:57 +0800

    first commit

---
+++ lib/data.dart
@@ -0,0 +1,27 @@
+class Course {
+  static final courses = [
+    Course(
+      title: 'Information Dart',
+      image: 'assets/vga.png',
+      time: '10 H 17 M',
+      description: 'Information on Dart basics.',
+    ),
+    Course(
+      title: 'Clean Spam',
+      image: 'assets/vga.png',
+      time: '5 H 29 M',
+      description: 'Clean Your Spam Email',
+    ),
+  ];
+  final String title;
+  final String image;
+  final String time;
+  final String description;
+
+  Course({
+    required this.title,
+    required this.image,
+    required this.time,
+    required this.description,
+  });
+}
